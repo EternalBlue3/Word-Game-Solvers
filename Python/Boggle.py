90647fb64cb8646517689746a70ae0e5f7d991bc
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2023-04-27 12:21:51 -0400

    Update Boggle.py

--- Python/Boggle.py
+++ Python/Boggle.py
@@ -1,13 +1,9 @@
-import logging, time
+import time
 
 X, Y = 4, 4
 
 def get_grid(grid):
-    return_dict = {}
-    for x in range(X):
-        for y in range(Y):
-            return_dict.update({(x,y):str(grid[x*X+y])})
-    return return_dict
+    return {(index // X, index % Y): str(value) for index, value in enumerate(grid)}
 
 def get_neighbours():
     neighbours = {}
@@ -23,7 +19,6 @@ def path_to_word(path):
 
 def search(path):
     word = path_to_word(path)
-    logging.debug('%s: %s' % (path, word))
     if word not in stems:
         return
     if word in dictionary:
@@ -31,8 +26,6 @@ def search(path):
     for next_pos in neighbours[path[-1]]:
         if next_pos not in path:
             search(path + [next_pos])
-        else:
-            logging.debug('skipping %s because in path' % grid[next_pos])
 
 def get_dictionary():
     stems, dictionary = set(), set()
@@ -48,20 +41,22 @@ def get_dictionary():
 
 def get_words():
     for position in grid:
-        logging.info('searching %s' % str(position))
         search([position])
     return [path_to_word(p) for p in paths]
 
 def print_grid(grid):
-    s = '\n'.join([' '.join(grid[x, y] for y in range(Y)) for x in range(X)]) + '\n'
-    print(s)
+    print('\n'.join([' '.join(grid[x, y] for y in range(Y)) for x in range(X)]) + '\n')
 
 def word_score(word):
     score = {1: 0, 2: 0, 3: 1, 4: 1, 5: 2, 6: 3, 7: 5}
     return score.get(len(word), 11)
 
+# Get board from user
 board = ' '.join(input("Enter the grid values: ").upper().split()).split()
+
+# Start timing
 start = time.time()
+
 grid = get_grid(board)
 neighbours = get_neighbours()
 dictionary, stems = get_dictionary()
