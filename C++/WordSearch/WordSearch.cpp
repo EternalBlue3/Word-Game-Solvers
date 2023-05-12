84f442baf89733299f0411d376c0b74fcadb7ff2
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2023-05-12 15:39:30 -0400

    Update WordSearch.cpp

--- C++/WordSearch/WordSearch.cpp
+++ C++/WordSearch/WordSearch.cpp
@@ -1,89 +1,98 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
-#include <cstdlib>
-#include <algorithm>
 #include <utility>
-#include <tuple>
+#include <chrono>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
 vector<string> read_gameboard() {
-    try {
-        ifstream file("game.txt");
-        if (!file.is_open()) {
-            throw runtime_error("Could not find the game file. Please create a file called \"game.txt\" in the same directory as this solver.");
-        }
-        vector<string> gameboard;
+    vector<string> gameboard;
+    ifstream f("game.txt");
+    if (f.is_open()) {
         string row;
-        while (getline(file, row)) {
+        while (getline(f, row)) {
             gameboard.push_back(row);
         }
-        for (string& row : gameboard) {
-            for (char& c : row) {
-                c = tolower(c);
-            }
-        }
-        return gameboard;
-    } catch (exception& e) {
-        cerr << e.what() << endl;
+    } else {
+        cerr << "Could not find the game file. Please create a file called \"game.txt\" in the same directory as this solver." << endl;
         exit(1);
     }
-}
-
-std::pair<bool, tuple<int, int, int, int>> check_horizontal_vertical(std::vector<std::string>& gameboard, std::string word) {
-    // Check rows
+    f.close();
     for (int i = 0; i < gameboard.size(); i++) {
-        if (gameboard[i].find(word) != std::string::npos) {
-            int j = gameboard[i].find(word);
-            return std::make_pair(true, make_tuple(i, j, i, j + word.length() - 1));
-        } else if (gameboard[i].find(std::string(word.rbegin(), word.rend())) != std::string::npos) {
-            int j = gameboard[i].find(std::string(word.rbegin(), word.rend()));
-            return std::make_pair(true, make_tuple(i, j, i, j + word.length() - 1));
+        for (int j = 0; j < gameboard[i].size(); j++) {
+            gameboard[i][j] = tolower(gameboard[i][j]);
         }
     }
-
-    // Check columns
-    for (int j = 0; j < gameboard[0].length(); j++) {
-        std::string column;
-        for (int i = 0; i < gameboard.size(); i++) {
-            column.push_back(gameboard[i][j]);
-        }
-        if (column.find(word) != std::string::npos) {
-            int i = column.find(word);
-            return std::make_pair(true, make_tuple(i, j, i + word.length() - 1, j));
-        } else if (column.find(std::string(word.rbegin(), word.rend())) != std::string::npos) {
-            int i = column.find(std::string(word.rbegin(), word.rend()));
-            return std::make_pair(true, make_tuple(i, j, i + word.length() - 1, j));
-        }
-    }
-
-    return std::make_pair(false, make_tuple(0, 0, 0, 0));
+    return gameboard;
 }
 
-void print_board(vector<string>& gameboard, vector<int>& coords, string& word) {
+vector<pair<int, int>> check_dir(const vector<string>& gameboard, const string& word, const pair<int, int>& direction, const int& lengthx, const int& lengthy) {
+    int dx = direction.first, dy = direction.second;
+    vector<pair<int, int>> coords;
+    for (int y = 0; y < lengthy; y++) {
+        for (int x = 0; x < lengthx; x++) {
+            bool match = true;
+            for (int i = 0; i < word.length(); i++) {
+                int xi = x + i * dx, yi = y + i * dy;
+                if (!(0 <= xi && xi < lengthx && 0 <= yi && yi < lengthy) || gameboard[yi][xi] != word[i]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                for (int i = 0; i < word.length(); i++) {
+                    int xi = x + i * dx, yi = y + i * dy;
+                    coords.push_back({xi, yi});
+                }
+                return coords;
+            }
+        }
+    }
+    return {};
+}
+
+pair<bool, vector<pair<int, int>>> find_word(const vector<string>& gameboard, const string& word) {
+    vector<pair<int, int>> dirs = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+    int lengthx = gameboard[0].length(), lengthy = gameboard.size();
+    for (auto direction : dirs) {
+        vector<pair<int, int>> coords = check_dir(gameboard, word, direction, lengthx, lengthy);
+        if (!coords.empty()) {
+            return {true, coords};
+        }
+    }
+    return {false, {}};
+}
+
+void print_board(vector<string>& gameboard, vector<pair<int,int>>& coords, string& word) {
     const string RED = "\033[31m";  // red text color
     const string END = "\033[0m";     // end color
     
-    cout << "\n\"" << word << "\" found at: (" << coords[0] << "," << coords[1] << ") to (" << coords[2] << "," << coords[3] << ")\n";
-    cout << "   ";
-    for (int i = 0; i < gameboard[0].size(); i++) {
-        cout << i % 10 << " ";
+    std::set<std::pair<int, int>> coord_set(coords.begin(), coords.end());
+    
+    std::cout << "\n\"" << word << "\" found at coords: ";
+    for (const auto& coord : coords) {
+        std::cout << "(" << coord.first << ", " << coord.second << ") ";
     }
-    cout << endl;
+    std::cout << "\n    ";
+    for (int i = 0; i < gameboard[0].size(); i++) {
+        std::cout << i % 10 << " ";
+    }
+    std::cout << "\n";
     for (int i = 0; i < gameboard.size(); i++) {
-        cout << i << "  ";
+        std::string line = std::to_string(i) + " " + std::string(std::to_string(gameboard.size()).length() - std::to_string(i).length() + 1, ' ');
         for (int j = 0; j < gameboard[i].size(); j++) {
-            if (coords[0] <= i && i <= coords[2] && coords[1] <= j && j <= coords[3]) {
-                cout << RED << gameboard[i][j] << END << " ";
+            if (coord_set.count({j, i}) != 0) {
+                line += RED + gameboard[i][j] + END + " ";
             } else {
-                cout << gameboard[i][j] << " ";
+                line += gameboard[i][j];
+                line += " ";
             }
         }
-        cout << endl;
+        std::cout << line << "\n";
     }
-    cout << endl;
 }
 
 int main() {
@@ -100,13 +109,21 @@ int main() {
           exit(0);
       }
       
-      std::pair<bool, tuple<int, int, int, int>> output = check_horizontal_vertical(gameboard,word);
-      if (output.first) {
-          vector<int> coordinates{std::get<0>(output.second),std::get<1>(output.second),std::get<2>(output.second),std::get<3>(output.second)};
-          print_board(gameboard,coordinates,word);
+      auto start = chrono::high_resolution_clock::now();
+      std::pair<bool, vector<pair<int, int>>> output = find_word(gameboard, word);
+      auto end = chrono::high_resolution_clock::now();
+      
+      if (!output.first) {
+          cout << "\"" << word << "\" not found." << endl << endl;
+          continue;
       } else {
-          cout << "Word not found." << "\n\n";
+          vector<pair<int,int>> coords = output.second;
+          print_board(gameboard,coords,word);
+          
+          auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+          std::cout << "\nSearch took " << duration.count() << "μs to complete.\n"; //Print time taken to complete search
       }
+      
   }
 
   return 0;
